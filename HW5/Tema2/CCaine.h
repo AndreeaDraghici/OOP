159797a712339ce11627bbbece6a409db5b01e6e
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-25 19:28:34 +0300

    Create CCaine.h

---
+++ HW5/Tema2/CCaine.h
@@ -0,0 +1,17 @@
+#pragma once
+class CCaine
+{
+protected:
+
+	char nume[10];
+    int inaltime;
+	int greutate;
+	int varsta;
+	char culoare[10];
+
+ public:
+	
+	void Citire() ;
+	void Afisare() ;
+};
+
