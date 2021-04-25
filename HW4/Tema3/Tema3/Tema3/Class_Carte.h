6ddebac724d4bd9063058711b99ca751c7059b3b
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-25 19:27:27 +0300

    Create Class_Carte.h

---
+++ HW4/Tema3/Tema3/Tema3/Class_Carte.h
@@ -0,0 +1,16 @@
+#pragma once
+class Class_Carte
+{
+   public:
+
+		char titlu[40];
+		char autor1[40];
+		char autor2[40];
+		char editura[40];
+		char nr_ISBN[40];
+		char anul_aparitiei[40];
+		float pret;
+
+		void citeste_carte();
+		void afiseaza_carte();
+};
