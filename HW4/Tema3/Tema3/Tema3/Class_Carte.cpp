202a97d105ce01b64c21e5c0fc2f5792de5c95dd
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-25 19:27:24 +0300

    Create Class_Carte.cpp

---
+++ HW4/Tema3/Tema3/Tema3/Class_Carte.cpp
@@ -0,0 +1,45 @@
+#include "Class_Carte.h"
+
+#include<iostream>
+
+using namespace std;
+
+void Class_Carte::citeste_carte()
+{
+	cout << "Introduceti titlul cartii:";
+	cin >> titlu;
+
+	cout << "Introduceti prenumele autorului:";
+	cin >> autor1;
+
+	cout << "Introduceti numele autorului:";
+	cin >> autor2;
+
+	cout << "Introduceti editura:";
+	cin >> editura;
+
+	cout << "Introduceti anul aparitiei:";
+	cin >> anul_aparitiei;
+
+	cout << "Introduceti pretul:";
+	cin >> pret;
+
+	cout << endl;
+}
+
+
+void Class_Carte::afiseaza_carte()
+{
+	cout << "Titlul cartii este: " << titlu << endl;
+
+	cout << "Penumele autorului este: " << autor1 << endl;
+
+	cout << "Numele autorului este: " << autor2 << endl;
+
+	cout << "Editura este: " << editura << endl;
+
+	cout << "Anul aparitiei este: " << anul_aparitiei << endl;
+
+	cout << "Pretul este: " << pret << endl;
+	cout << endl;
+}
